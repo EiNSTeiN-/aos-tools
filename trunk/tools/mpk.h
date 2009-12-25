9b1c98c71c1b20ed842a7de5630bae7e452579d8
EiNSTeiN.1337 <EiNSTeiN.1337@09a0fd3e-c781-11de-a9c8-2fbcfba63733>
2009-12-25 02:01:34 +0000

    Added Archos 3G+ support

--- trunk/tools/mpk.h
+++ trunk/tools/mpk.h
@@ -15,42 +15,50 @@
 
 #define MPK_DEVICE_A5			0
 #define MPK_DEVICE_A5IT		1
+#define MPK_DEVICE_A3GP		2
 
-#define MPK_KNOWN_DEVICES		2 // number of keys in each array
+#define MPK_KNOWN_DEVICES		3 // number of keys in each array
 
 static unsigned char *AES_Keys[] = {
 	A5_AES,
 	A5IT_AES,
+	A3GP_AES,
 };
 
 static unsigned char *Bootloader_Keys[] = {
 	A5_BOOTLOADER,
 	A5IT_BOOTLOADER,
+	A3GP_BOOTLOADER,
 };
 
 static unsigned char *Rel_Keys[] = {
 	A5_RELMPK,
 	A5IT_RELMPK,
+	A3GP_RELMPK,
 };
 
 static unsigned char *Dev_Keys[] = {
 	A5_DEVMPK,
 	A5IT_DEVMPK,
+	A3GP_DEVMPK,
 };
 
 static unsigned char *Plug_Keys[] = {
 	A5_PLUGMPK,
 	A5IT_PLUGMPK,
+	A3GP_PLUGMPK,
 };
 
 static unsigned char *HDD_Keys[] = {
 	A5_HDDMPK,
 	A5IT_HDDMPK,
+	A3GP_HDDMPK,
 };
 
 static unsigned char *Games_Keys[] = {
 	A5_GAMESMPK,
 	A5IT_GAMESMPK,
+	A3GP_GAMESMPK,
 };
 
 unsigned char **mpk_possible_aos_keys(uint32_t sign_type);
